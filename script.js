623bf2d24adaae0320c2b011eaf5e7661858b42e
Vyga <vygintaskirda@gmail.com>
2023-03-09 10:21:57 +0200

    Building audio

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ import myElementCreator from './tools/myElementCreator.js'
 
 const allLtLettersArr = ['Aa', 'Ąą', 'Bb', 'Cc', 'Čč', 'Dd', 'Ee', 'Ęę', 'Ėė', 'Ff', 'Gg', 'Hh', 'Ii', 'Įį', 'Yy', 'Jj', 'Kk', 'Ll', 'Mm', 'Nn', 'Oo', 'Pp', 'Rr', 'Ss', 'Šš', 'Tt', 'Uu', 'Ųų', 'Ūū', 'Vv', 'Zz', 'Žž']
 const capsLTLettersArr = allLtLettersArr.toString().replace(/[a-ząčęėįšųūž]/g,'').split(',')
-console.log(capsLTLettersArr)
 const capsLatinLTLettersArr = allLtLettersArr.toString().replace(/[^A-Z]/g,'').split('')
-console.log(capsLatinLTLettersArr)
-
 
 
 myElementCreator('div', 'content-wrap')
@@ -13,12 +10,40 @@ myElementCreator('div', 'content-wrap')
 let mainTitle = myElementCreator('h1', 'main-title','content-wrap')
 mainTitle.textContent='KAJOS RAIDĖS'
 
-let letterWrap = myElementCreator('ul', 'letter-wrap','content-wrap')
+myElementCreator('ul', 'letter-wrap','content-wrap')
 
-capsLatinLTLettersArr.map((element)=>{
-    let oneElement = myElementCreator('li',[element,'letter'],'letter-wrap')
-    oneElement.textContent = element
-    console.log(element)
 
+
+
+mainTitle.addEventListener('click',()=>{
+    let audio = myElementCreator('audio','letterA')
+    audio.src='/audioLetter/A.mp3'
+    audio.play()
+})
+
+
+
+
+
+
+
+const randomLetter = capsLatinLTLettersArr[Math.floor(Math.random() * capsLatinLTLettersArr.length)];
+console.log('cia:',randomLetter)
+
+capsLatinLTLettersArr.map((letter)=>{
+    let oneLetter = myElementCreator('li',[letter,'letter'],'letter-wrap')
+    oneLetter.textContent = letter
+
+    oneLetter.addEventListener('click',()=>{
+        console.log('click '+ letter)
+
+        if (randomLetter === letter){
+            console.log('hit')
+            oneLetter.classList.add('hit')
+        } else {
+            console.log('miss')
+            oneLetter.classList.add('miss')
+        }
+    })
 })
 
