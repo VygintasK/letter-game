94c91a6ac288d2438b9980375dfd11c64ea90f72
Vyga <vygintaskirda@gmail.com>
2023-03-31 12:05:50 +0300

    Chnages in script With GDP advice

--- script.js
+++ script.js
@@ -3,74 +3,79 @@ import myElementCreator from './tools/myElementCreator.js'
 import innerScreenScan from './js/innerScreenScan.js'
 import makeRandomLetter from './js/makeRandomLetter.js'
 import playAudioOnClick from './js/playAudioOnClick.js'
+// import letterClickHandler from './js/letterClickHandler.js'
 
 
 const allLtLettersArr = ['Aa', 'Ąą', 'Bb', 'Cc', 'Čč', 'Dd', 'Ee', 'Ęę', 'Ėė', 'Ff', 'Gg', 'Hh', 'Ii', 'Įį', 'Yy', 'Jj', 'Kk', 'Ll', 'Mm', 'Nn', 'Oo', 'Pp', 'Rr', 'Ss', 'Šš', 'Tt', 'Uu', 'Ųų', 'Ūū', 'Vv', 'Zz', 'Žž']
+//removes small and LT
 // const capsLTLettersArr = allLtLettersArr.toString().replace(/[a-ząčęėįšųūž]/g,'').split(',')
-const capsLatinLTLettersArr = allLtLettersArr.toString().replace(/[^A-Z]/g,'').split('')
-// const capsLatinLTLettersArr =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'Y', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'Z']
+const capsLatinLTLettersArr = allLtLettersArr.toString().replace(/[^A-Z]/g, '').split('')
+
 
 // HEADER
-let header = myElementCreator('header','header')
-let headerContentWrap = myElementCreator('div','headerContentWrap','header')
-let winScore=0
-let looseScore=0
-let ScoreElm = myElementCreator('div','score','headerContentWrap')
-let winElm = myElementCreator('span','win','score')
-let separatorElm = myElementCreator('span','separator','score')
-let looseElm = myElementCreator('span','loose','score')
+myElementCreator('header', 'header')
+const headerContentWrap = myElementCreator('div', 'headerContentWrap', 'header')
+
+myElementCreator('div', 'score', 'headerContentWrap')
+let winElm = myElementCreator('span', 'win', 'score')
+let winScore = 0
 winElm.textContent = winScore
+winElm.style.color = '#66ff00'
+let separatorElm = myElementCreator('span', 'separator', 'score')
 separatorElm.textContent = '-'
+let looseElm = myElementCreator('span', 'loose', 'score')
+let looseScore = 0
 looseElm.textContent = looseScore
-winElm.style.color='#66ff00'
-looseElm.style.color='red'
-let mainTitle = myElementCreator('h2', 'main-title','headerContentWrap')
-let soundSVG = myElementCreator('img','soundSVG','headerContentWrap')
-mainTitle.textContent='PASPAUSK'
-soundSVG.src ='./img/sound.svg'
+looseElm.style.color = 'red'
+
+const mainTitle = myElementCreator('h2', 'main-title', 'headerContentWrap')
+mainTitle.textContent = 'PASPAUSK'
+
+const soundSVG = myElementCreator('img', 'soundSVG', 'headerContentWrap')
+soundSVG.src = './img/sound.svg'
 
 
+renderLetters()
+innerScreenScan()
 
-function renderLetters(){
 
+function renderLetters() {
     const randomLetter = makeRandomLetter(capsLatinLTLettersArr)
-
-    playAudioOnClick(randomLetter,headerContentWrap)
-
+    playAudioOnClick(randomLetter, headerContentWrap)
 
     // LETTERS
-    let contentWrap = myElementCreator('div', 'content-wrap')
-    myElementCreator('ul', 'letter-wrap','content-wrap')
-    
-    capsLatinLTLettersArr.map((oneLetter)=>{
-        let oneLetterElm = myElementCreator('li',[oneLetter,'letter'],'letter-wrap')
-        oneLetterElm.textContent = oneLetter
+    const contentWrap = myElementCreator('div', 'content-wrap')
+    myElementCreator('ul', 'letter-wrap', 'content-wrap')
 
-       
-        oneLetterElm.addEventListener('click',()=>{
-            if (randomLetter === oneLetter){
-                oneLetterElm.classList.add('hit')
-                winScore++
-                winElm.textContent = winScore
-                let screenBlock = myElementCreator('div','screenBlock')
-                let screenBlockImg = myElementCreator('img','screenBlockImg','screenBlock')
-                screenBlockImg.src = './img/ryder.png'
-                setTimeout(function() {
-                    contentWrap.remove()
-                    screenBlock.remove()
-                    renderLetters()
-                }, 2000);
-            } else {
-                oneLetterElm.classList.add('miss')
-                looseScore++
-                looseElm.textContent = looseScore
-            }
-        })
-        
+    capsLatinLTLettersArr.forEach((oneLetter) => {
+        const oneLetterElm = myElementCreator('li', [oneLetter, 'letter'], 'letter-wrap')
+        oneLetterElm.textContent = oneLetter
+        const clickHandlerOBJ={
+            oneLetterElm,
+            randomLetter,
+            oneLetter,
+            contentWrap
+        }
+        letterClickHandler(clickHandlerOBJ)
     })
 }
-renderLetters()
+function letterClickHandler(clickHandlerOBJ) {
+    const {oneLetterElm,randomLetter,oneLetter,contentWrap} = clickHandlerOBJ
+    oneLetterElm.addEventListener('click', () => {
 
-
-//  SCREEN
-innerScreenScan()
+        const isCorrect = randomLetter === oneLetter
+        winElm.textContent = isCorrect ? ++winScore : winScore;
+        looseElm.textContent = isCorrect ? looseScore : ++looseScore;
+        oneLetterElm.classList.add(isCorrect ? 'hit' : 'miss')
+        if (isCorrect) {
+            let screenBlock = myElementCreator('div', 'screenBlock')
+            let screenBlockImg = myElementCreator('img', 'screenBlockImg', 'screenBlock')
+            screenBlockImg.src = './img/ryder.png'
+            setTimeout(() => {
+                contentWrap.remove()
+                screenBlock.remove()
+                renderLetters()
+            }, 2000);
+        }
+    })
+}
